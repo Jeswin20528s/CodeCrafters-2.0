fdd1ed922eb3dc894470415d7311d5260e33e677
Jeswin20528s <jeswinjustin7221@gmail.com>
2024-10-09 15:21:10 +0000

    good

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-'''
-num_1: int = int(input("enter a number 1; "))
-num_2: int = int(input("enter a number 2; "))
+
+num_1: int = int(input("enter a number 1: "))
+num_2: int = int(input("enter a number 2: "))
 
 if num_1 == num_2:
     print("both are same")
 else:
     print("both are diff")
-'''    
+   
